f70dbeab8e8035ff913b48cfa9f37b93a71a2e2b
Jeff Bowen <jblz@users.noreply.github.com>
2019-05-21 16:30:06 -0400

    Add script descs, wordcount example, fn name

--- js/demo1.js
+++ js/demo1.js
@@ -1,4 +1,4 @@
-( async function() {
+( function( { autop, element, escapeHTML, i18n, nux, wordcount } ) {
 	// src: https://stackoverflow.com/a/23234031/5883953
 	function isElementInViewport( el ) {
 		const rect = el.getBoundingClientRect();
@@ -6,17 +6,28 @@
 	}
 
 	const el = document.querySelector( 'article .entry-content' );
-	el.innerHTML = wp.autop.autop(
-		`These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.
+
+	const contentMabyeFromAnAPI = `These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.
 
 	Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
 
-	I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.` );
+	I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
+
+	`;
+
+	// @TODO escape HTML in contentMabyeFromAnAPI
+
+	let contentHTML = autop.autop( contentMabyeFromAnAPI );
+	const numWords = wordcount.count( contentMabyeFromAnAPI, 'words' );
+
+	contentHTML += `This high-quality content has ${ numWords } words!`;
+
+	el.innerHTML = contentHTML;
 
 	const btn = document.createElement( 'button' );
 
 	// Wrap UI components with translation functions
-	const { __ } = wp.i18n;
+	const { __ } = i18n;
 	btn.textContent = __( 'Engage!' );
 
 	const wrapper = document.createElement( 'div' );
@@ -38,12 +49,12 @@
 		clearInterval( vizInterval );
 
 		setTimeout( () => {
-			wp.element.render(
-				wp.element.createElement( wp.nux.DotTip, {
+			element.render(
+				element.createElement( nux.DotTip, {
 					tipId: 'jbwca19-button-demo1'
 				}, __( 'Boldly go where no one has gone before!' ) ),
 				tipTarget
 			);
 		}, 700 );
 	}, 200 );
-} )();
+} )( wp );
