df0644757c23de99fa64edcb0368c03d67700339
Jeff Bowen <jblz@users.noreply.github.com>
2019-06-03 17:52:38 -0400

    got the formatter working

--- src/demo2/index.js
+++ src/demo2/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Demo2 = ( props = {} ) => {
-	return (
-		<div>
-			Hello world!
-		</div>
-	);
-}
+	return <div>Hello world!</div>;
+};
 
 export default Demo2;
